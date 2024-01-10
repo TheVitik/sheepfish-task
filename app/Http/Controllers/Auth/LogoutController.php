74ca2792a21a03d2eb2ea758679260562d179e84
TheVitik <lokplay2003@gmail.com>
2024-01-10 19:47:24 +0200

    Add Laravel Auth Add Breeze starter kit Add seeder to create first user

---
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+class LogoutController extends Controller
+{
+    public function logout(): RedirectResponse
+    {
+        auth()->logout();
+        session()->invalidate();
+        session()->regenerateToken();
+
+        return redirect()->route('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\PasswordController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +20,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('login', [LoginController::class, 'login'])->name('login');
+
+    Route::post('login', [LoginController::class, 'store']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::put('/password', [PasswordController::class, 'update'])->name('password.update');
+
+    Route::post('/logout', [LogoutController::class, 'logout'])
+        ->name('logout');
 });
